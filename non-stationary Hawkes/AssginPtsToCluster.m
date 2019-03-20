220f9aa8fa00c517172f06bf3fe42d29069d6903
JoeEm <46802975+JoeEm@users.noreply.github.com>
2019-03-20 11:36:00 +0800

    add clusteredFunction Declaration

---
+++ non-stationary Hawkes/AssginPtsToCluster.m
@@ -0,0 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% AssginPtsToCluster() : The function receives the orignial Data , 
+% then Assgins the points to corresponding cluster via Algorithnm 1,
+% and finally return the ClusteredData
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function ClusteredData = AssginPtsToCluster( OriginalData, ClusterNumbers)
+
+end
